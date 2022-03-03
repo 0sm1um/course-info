d34726255fcf20a0a9240ce3136b88b3e32a715b
john.hiles <john.hiles@wright.edu>
2022-03-03 14:24:58 -0500

    Committing recent materials.

--- STAT/STAT-636 Machine Learning Algorithms/HW03/RCode.R
+++ STAT/STAT-636 Machine Learning Algorithms/HW03/RCode.R
@@ -1,2 +1,12 @@
-setwd("C:/Users/echo4/Class/STAT/STAT-636 Machine Learning Algorithms/HW01")
-load("crx.new.data")
+setwd("C:/Users/echo4/Class/STAT/STAT-636 Machine Learning Algorithms/HW03")
+mydata <- read.table("GeneData_first15.csv", row.names = 1)
+
+
+mypca <- prcomp(mydata[,c(1:15)], center = TRUE,scale. = TRUE)
+summary(mypca)
+
+myvar <- mypca$sdev^2
+myvar_scaled <- myvar/sum(myvar)
+
+barplot(myvar_scaled)
+
